9059708b5a8008f775a29b8bd9eb95e79dbfc6bd
feranicus <feranicus@gmail.com>
2025-10-06 19:36:44 +0100

    Finalize Vite config and deployment workflow

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/mypageres/', // <--- ДОБАВЬТЕ ЭТУ СТРОКУ
+  // Change this line
+  base: '/', 
   plugins: [react()],
 })
