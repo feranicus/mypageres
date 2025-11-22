9d80da7d62ef6fc020e35541f4b7c0beaa71d37f
feranicus <feranicus@gmail.com>
2025-11-22 12:28:31 +0000

    fix: Corrected Vite base path for mypageres GitHub Pages deployment.

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  // !!! ИСПРАВЛЕНИЕ ДЛЯ GITHUB PAGES !!!
+  base: '/mypageres/', 
+  
   plugins: [react()],
   build: {
     target: 'esnext'
